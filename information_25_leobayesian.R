01ce21f30a50cde92975797f96d6f580ebde7dd7
Leo <leotiokhin@gmail.com>
2018-10-13 07:39:17 -0700

    updated with data from RR

--- information_25_leobayesian.R
+++ information_25_leobayesian.R
@@ -66,3 +66,63 @@ simplehist(tiles_revealed,
                         Players guess with", threshold_u, 
                         "confidence; Mean Tiles=", mean(tiles_revealed)))
 
+#############
+###Data from RR study (no excluding individual observations)
+############
+
+###Load Data###
+
+sequences <- read.table("Sequences1.txt", header=FALSE) #the sequence of blue and yellow tiles for each grid
+
+###Cleaning and Data Prep###
+d.conf_1_2.c <- d.conf_1_2[complete.cases(d.conf_1_2),]
+
+#exclude participants who indicated technical difficulties
+d.conf_1_2.c <- d.conf_1_2.c[d.conf_1_2.c$ID_Player != 26,]
+d.conf_1_2.c <- d.conf_1_2.c[d.conf_1_2.c$ID_Player != 61,]
+
+###for each of the 600 possible sequences, the number of majority tiles (i.e. effect size)###
+seq.df <- data.frame(seq = sequences)
+t <- str_split(seq.df$V1, ";")
+t <- do.call(rbind, t)
+t <- as.data.frame(t)
+
+for(i in 1:ncol(t)){
+  t[,i] <- as.numeric(t[,i]) - 1
+}
+
+#number of majority tiles for each grid
+n_majority <- apply(t, 1, sum) 
+
+#identifies which grids correspond to which effect size
+g_13 <- which(n_majority == 13)
+g_15 <- which(n_majority == 15)
+g_17 <- which(n_majority == 17)
+
+#adds this to relevant data sets
+d.conf_1_2.c$n_major <- rep(NA, nrow(d.conf_1_2.c))
+
+for(i in 1:nrow(d.conf_1_2.c)){
+  if(d.conf_1_2.c$Guess_Number[i] %in% g_13){d.conf_1_2.c$n_major[i] <- 13}
+  if(d.conf_1_2.c$Guess_Number[i] %in% g_15){d.conf_1_2.c$n_major[i] <- 15}
+  if(d.conf_1_2.c$Guess_Number[i] %in% g_17){d.conf_1_2.c$n_major[i] <- 17}
+}
+
+#Aggregate data for each participant
+d.conf.agg <- aggregate(TilesRevealed ~ Effort + Competition+
+                          Guess_Number + n_major + ID_Player, data=d.conf_1_2.c, FUN = mean)
+
+#separate plots of tiles for each condition
+hist(d.conf.agg$TilesRevealed[d.conf.agg$Effort==0 & d.conf.agg$Competition==0])
+hist(d.conf.agg$TilesRevealed[d.conf.agg$Effort==0 & d.conf.agg$Competition==1])
+hist(d.conf.agg$TilesRevealed[d.conf.agg$Effort==1 & d.conf.agg$Competition==0])
+hist(d.conf.agg$TilesRevealed[d.conf.agg$Effort==1 & d.conf.agg$Competition==1])
+
+#separate objects just based on competition (not segregating on effort)
+df.comp <- d.conf.agg$TilesRevealed[d.conf.agg$Competition==0]
+df.nocomp <- d.conf.agg$TilesRevealed[d.conf.agg$Competition==1]
+
+
+
+
+
